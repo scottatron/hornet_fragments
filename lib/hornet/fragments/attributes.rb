696c88a3c948ba711ce50635e98e5c86255557ba
Scott Arthur <scott@scottatron.com>
2014-06-20 12:31:08 +1000

    Set autosave: true on fragment_accessor has_one association

--- lib/hornet/fragments/attributes.rb
+++ lib/hornet/fragments/attributes.rb
@@ -9,7 +9,7 @@ module Hornet
 
         def fragment_accessor(attribute, type = :string)
 
-          has_one "#{attribute}_fragment".to_sym, as: :fragmentable, class_name: 'Fragment'
+          has_one "#{attribute}_fragment".to_sym, as: :fragmentable, class_name: 'Fragment', autosave: true
 
           instance_methods = Module.new do
 
